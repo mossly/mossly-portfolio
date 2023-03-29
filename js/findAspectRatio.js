4b3272a147db08bdeb82d2d76ee8ca78d47ce78a
mossly <moss.aaron@icloud.com>
2023-03-29 20:25:49 +1300

    Delete findAspectRatio.js

--- js/findAspectRatio.js
+++
@@ -1,6 +0,0 @@
-function setAspectRatio(img) {
-  const aspectRatio = img.naturalHeight / img.naturalWidth;
-  const paddingBottom = aspectRatio * 100;
-
-  img.parentElement.style.paddingBottom = `${paddingBottom}%`;
-}
